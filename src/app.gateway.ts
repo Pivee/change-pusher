9912524f7ce6b8ceacd01d8d7038a0cbba6913d9
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-06-18 13:04:36 +0530

    Connect Socket.io

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,7 +1,15 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 
 @WebSocketGateway()
-export class AppGateway {
+export class AppGateway implements OnGatewayConnection {
+  handleConnection(client: any, ...args: any[]) {
+    console.log('Change Pusher Client connected: ', client.id);
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
