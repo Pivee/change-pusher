e39528c2a90da11535055f8738e27101f1da1700
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-06-18 14:18:09 +0530

    Fix response speed issue

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,10 +1,11 @@
 import {
   OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { interval } from 'rxjs';
+import { Server } from 'socket.io';
 import {
   adjectives,
   animals,
@@ -12,25 +13,43 @@ import {
 } from 'unique-names-generator';
 
 @WebSocketGateway()
-export class AppGateway implements OnGatewayConnection {
+export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  clientPool = {};
+
   handleConnection(client: any, ...args: any[]) {
+    const { id: clientId } = client;
+
+    this.clientPool[clientId] = client;
+
     console.log('Change Pusher Client connected: ', client.id);
-    interval(2000)
-      .pipe()
-      .forEach(() => {
-        this.server.emit('GetNewAnimal', {
-          data: uniqueNamesGenerator({
-            dictionaries: [adjectives, animals],
-          }),
-        });
-      });
+    console.log(Object.keys(this.clientPool));
+
+    if (this.clientPool[clientId]) {
+      setInterval(() => {
+        client.emit('GetNewAnimal', this.getNewAnimal());
+      }, 2000);
+    }
   }
 
-  @WebSocketServer()
-  server;
+  handleDisconnect(client: any) {
+    const { id: clientId } = client;
+
+    delete this.clientPool[clientId];
+  }
 
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
+  }
+
+  private getNewAnimal() {
+    return {
+      data: uniqueNamesGenerator({
+        dictionaries: [adjectives, animals],
+      }),
+    };
   }
 }
