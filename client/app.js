06b0c71095273e59bbda8b9d02ec4ebb83cb9d57
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-06-18 13:43:01 +0530

    Implement Unique Animal push at every 2s

--- client/app.js
+++ client/app.js
@@ -1,3 +1,19 @@
 console.log('Change Pusher Client successfully started');
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3000', {
+  auth: {
+    token: '123',
+  },
+});
+
+let animal;
+
+socket.on('GetNewAnimal', ({ data }) => {
+  console.log('Data Received: ', data);
+  animal = data;
+
+  document.getElementById('animal').value = animal;
+  document.getElementById(
+    'last-updated-at',
+  ).innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
+});
--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -2,13 +2,32 @@ import {
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { interval } from 'rxjs';
+import {
+  adjectives,
+  animals,
+  uniqueNamesGenerator,
+} from 'unique-names-generator';
 
 @WebSocketGateway()
 export class AppGateway implements OnGatewayConnection {
   handleConnection(client: any, ...args: any[]) {
     console.log('Change Pusher Client connected: ', client.id);
+    interval(2000)
+      .pipe()
+      .forEach(() => {
+        this.server.emit('GetNewAnimal', {
+          data: uniqueNamesGenerator({
+            dictionaries: [adjectives, animals],
+          }),
+        });
+      });
   }
+
+  @WebSocketServer()
+  server;
 
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
