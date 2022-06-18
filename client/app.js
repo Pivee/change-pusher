bb304ae1da87dfb7e0d1820df5baf2b7e0ce9bc7
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2022-06-18 16:25:34 +0530

    Add Users block

--- client/app.js
+++ client/app.js
@@ -14,6 +14,25 @@ socket.on('GetNewAnimal', ({ data }) => {
 
   document.getElementById('animal').value = animal;
   document.getElementById(
-    'last-updated-at',
+    'animals-last-updated-at',
+  ).innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
+});
+
+socket.on('GetUsers', ({ data }) => {
+  console.log('Data Received: ', data);
+  const users = data;
+
+  const usersListElement = document.getElementById('users');
+
+  let names = [];
+
+  users.forEach((user) => {
+    names.push(user.name);
+  });
+
+  usersListElement.innerText = names.join(', ');
+
+  document.getElementById(
+    'users-last-updated-at',
   ).innerText = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
 });
